7239ad7b092942aa7b463ce7557921c6fb16a924
Wu Sung-Mao <Allen@Wus-MacBook-Pro.local>
2016-11-22 21:49:47 -0600

    fixed server read css file issue

--- server.js
+++ server.js
@@ -23,11 +23,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
+//app.use(express.static('/view/assets'));
+app.use(express.static(__dirname + '/view/assets'));
+
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs. 
 // ================================================================================
+
 
 require('./view/html-routes.js')(app);
 
