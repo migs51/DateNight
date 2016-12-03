e79ff01ce8dd36e8e81b6a4b8fc71cbda7d3f202
Damian Mandola <dmandola11@gmail.com>
2016-12-03 12:54:32 -0600

    try try try

--- controller/database.js
+++ controller/database.js
@@ -1,9 +1,9 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        'user': 'za6tl503zt9023zc',
-        'password': 'c2gocdczjsgguet5'
+        'host': 'localhost',
+        'user': 'root',
+        'password': ''
     },
 	'database': 'my_schema',
     'users_table': 'users'
