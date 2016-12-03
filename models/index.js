acb9f593c6bc841391f70066513d065ae1edeed1
Damian Mandola <dmandola11@gmail.com>
2016-12-03 13:49:30 -0600

    change to config

--- models/index.js
+++ models/index.js
@@ -8,7 +8,8 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable !== 'development') {
+//if (config.use_env_variable !== 'development') {
+if (true) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
