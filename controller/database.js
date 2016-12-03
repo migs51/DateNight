4192083da83693e9dcc3e594b8736aa4c61c4563
Damian Mandola <dmandola11@gmail.com>
2016-12-03 12:52:26 -0600

    changed login and host info

--- controller/database.js
+++ controller/database.js
@@ -1,9 +1,9 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': ''
+        'host': 'tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'user': 'za6tl503zt9023zc',
+        'password': 'c2gocdczjsgguet5'
     },
 	'database': 'my_schema',
     'users_table': 'users'
